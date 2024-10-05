744f3c76ce328fd0faf095ac632c8b86eb70f252
Ernest Guevarra <ernest@guevarra.io>
2024-10-05 07:55:45 +0100

    increase wait and timeout values

--- _render_slides.R
+++ _render_slides.R
@@ -2,8 +2,6 @@
 
 ## Load packages ----
 library(xaringan)
-library(pagedown)
-library(bookdown)
 
 
 ## Create directory for slide outputs ----
@@ -42,30 +40,32 @@ lapply(
 
 ## Render slides to PDF ----
 
-# html_files <- list.files(
-#   path = output_dir, pattern = "session[0-9]{1,2}.html", full.names = TRUE
-# )
+html_files <- list.files(
+  path = output_dir, pattern = "session[0-9]{1,2}.html", full.names = TRUE
+)
 
-# pdf_files <- file.path(
-#   output_dir, "pdf",
-#   c(
-#     "session1-getting-the-right-tools.pdf",
-#     "session10-portable-r-projects.pdf",
-#     "session11-automating-r-projects.pdf",
-#     "session2-r-basics-part1.pdf",
-#     "session3-r-basics-part2.pdf",
-#     "session4-r-basics-part3.pdf",
-#     "session5-literate-programming.pdf",
-#     "session6-open-reproducible-science.pdf",
-#     "session7-git-and-github-with-r.pdf",
-#     "session8-reproducible-scientific-workflows.pdf",
-#     "session9-reproducible-scientific-workflows.pdf"
-#     )
-# )
+pdf_files <- file.path(
+  output_dir, "pdf",
+  c(
+    "session1-getting-the-right-tools.pdf",
+    "session10-portable-r-projects.pdf",
+    "session11-automating-r-projects.pdf",
+    "session2-r-basics-part1.pdf",
+    "session3-r-basics-part2.pdf",
+    "session4-r-basics-part3.pdf",
+    "session5-literate-programming.pdf",
+    "session6-open-reproducible-science.pdf",
+    "session7-git-and-github-with-r.pdf",
+    "session8-reproducible-scientific-workflows.pdf",
+    "session9-reproducible-scientific-workflows.pdf"
+    )
+)
 
-# Map(
-#   f = pagedown::chrome_print,
-#   input = html_files,
-#   output = pdf_files,
-#   extra_args = "--no-sandbox"
-# )
+Map(
+  f = pagedown::chrome_print,
+  input = html_files,
+  output = pdf_files,
+  wait = 100,
+  timeout = 300,
+  extra_args = "--no-sandbox"
+)
